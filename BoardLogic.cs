fa71797b4898d046d96b43aa6f0ac93249b36ca8
FlamingForge <perickson@student.neumont.edu>
2024-02-15 19:24:57 -0700

    up fix

--- BoardLogic.cs
+++ BoardLogic.cs
@@ -611,7 +611,7 @@ namespace Pente_WPFApp
                 int currentValue = 0;
                 if (row < boardSize && column - i >= 0)
                 {
-                    if ((row - i) < 0) { break; }
+                    if ((column - i) < 0) { break; }
                     currentValue = board[(row), (column - i)];
                 }
                 if (currentValue == 0 || currentValue == 2) { break; }
